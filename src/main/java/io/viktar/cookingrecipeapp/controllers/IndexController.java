aa33cf90c550c987aa55b8373eb7d376f07db20f
viktar-b <viktar.beliakou@gmail.com>
2021-03-03 18:54:55 +0000

    project initiated

---
+++ src/main/java/io/viktar/cookingrecipeapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package io.viktar.cookingrecipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/", " "})
+    public String getIndexPage() {
+        return "index";
+    }
+}
---
+++ src/test/java/io/viktar/cookingrecipeapp/CookingRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.viktar.cookingrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookingRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
